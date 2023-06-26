94fcc5ec426ca1f087bc0d4b3f879ade3bfc6cf2
rei <jhallwachs36@gmail.com>
2023-06-26 11:54:32 +0000

    cringe

--- routes/cp.php
+++ routes/cp.php
@@ -4,17 +4,10 @@ use App\Http\Controllers\SettingsController;
 use App\Http\Controllers\DefaultsController;
 use Tags\MetatagsTags;
 
-
-// Laravel 9 way
 Route::view('metatags', 'statamic-metatags::index')->name('metatags.index');
 
+Route::get('metatags/settings', 'SettingsController@edit')->name('metatags.settings');
+Route::post('metatags/settings', 'SettingsController@update')->name('metatags.settings.update');
 
-
-// Laravel 9 way
-Route::get('metatags/settings', [SettingsController::class, 'edit'])->name('metatags.settings');
-Route::post('metatags/settings', [SettingsController::class, 'update'])->name('metatags.settings.update');
-
-
-// Laravel 9 way
-Route::get('metatags/defaults', [DefaultsController::class, 'edit'])->name('metatags.defaults');
-Route::post('metatags/defaults', [DefaultsController::class, 'update'])->name('metatags.defaults.update');
+Route::get('metatags/defaults', 'DefaultsController@edit')->name('metatags.defaults');
+Route::post('metatags/defaults', 'DefaultsController@update')->name('metatags.defaults.update');
