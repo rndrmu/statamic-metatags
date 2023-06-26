24aa07f1e5f51e668ec18cb184d3bbe0296a88bb
rei <jhallwachs36@gmail.com>
2023-06-26 12:05:29 +0000

    pls work :(

--- routes/cp.php
+++ routes/cp.php
@@ -1,10 +1,13 @@
 <?php
 
+use App\Http\Controllers\SettingsController;
+use App\Http\Controllers\DefaultsController;
+
 
 Route::view('metatags', 'statamic-metatags::index')->name('metatags.index');
 
-Route::get('metatags/settings', 'App\Http\Controllers\SettingsController@edit')->name('metatags.settings');
-Route::post('metatags/settings', 'App\Http\Controllers\SettingsController@update')->name('metatags.settings.update');
+Route::get('metatags/settings', [SettingsController::class, 'edit'])->name('metatags.settings');
+Route::post('metatags/settings', [SettingsController::class, 'update'])->name('metatags.settings.update');
 
-Route::get('metatags/defaults', 'App\Http\Controllers\DefaultsController@edit')->name('metatags.defaults');
-Route::post('metatags/defaults', 'App\Http\Controllers\DefaultsController@update')->name('metatags.defaults.update');
+Route::get('metatags/defaults', [DefaultsController::class, 'edit'])->name('metatags.defaults');
+Route::post('metatags/defaults', [DefaultsController::class, 'update'])->name('metatags.defaults.update');
