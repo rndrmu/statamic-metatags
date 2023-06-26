67e85dbae3419177763a7f19a05082475b84f4b9
rei <jhallwachs36@gmail.com>
2023-06-26 11:41:05 +0000

    help pls

--- routes/cp.php
+++ routes/cp.php
@@ -1,16 +1,18 @@
 <?php
 
-use rndrmu\StatamicMetatags\Http\Controllers\SettingsController;
-use rndrmu\StatamicMetatags\Http\Controllers\DefaultsController;
-use rndrmu\StatamicMetatags\Tags\MetatagsTags;
-
-// Laravel 9 way
-Route::view('metatags', 'metatags.index')->name('metatags.index');
+use Http\Controllers\SettingsController;
+use Http\Controllers\DefaultsController;
+use Tags\MetatagsTags;
 
 
 // Laravel 9 way
-Route::get('metatags/settings', [DefaultsController::class, 'edit'])->name('metatags.settings');
-Route::post('metatags/settings', [DefaultsController::class, 'update'])->name('metatags.settings.update');
+Route::view('metatags', 'statamic-metatags::index')->name('metatags.index');
+
+
+
+// Laravel 9 way
+Route::get('metatags/defaults', [DefaultsController::class, 'edit'])->name('metatags.defaults');
+Route::post('metatags/defaults', [DefaultsController::class, 'update'])->name('metatags.defaults.update');
 
 
 // Laravel 9 way
