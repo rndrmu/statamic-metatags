35dad4b40d9c7d43f1af485fb1d73e7c0bd164a6
rei <jhallwachs36@gmail.com>
2023-06-26 10:48:28 +0000

    amogus

--- routes/cp.php
+++ routes/cp.php
@@ -2,11 +2,18 @@
 
 use Http\Controllers\SettingsController;
 use Http\Controllers\DefaultsController;
+use Tags\MetatagsTags;
 
-Route::view('metatags', 'statamic-metatags::index')->name('metatags.index');
 
-Route::get('metatags/settings', [SettingsController::edit])->name('metatags.settings');
-Route::post('metatags/settings', [SettingsController::update])->name('metatags.settings.update');
+// Laravel 9 way
+Route::view('metatags', [MetatagsTags::class, 'index'])->name('metatags.index');
 
-Route::get('metatags/defaults', [DefaultsController::edit])->name('metatags.defaults');
-Route::post('metatags/defaults', [DefaultsController::update])->name('metatags.defaults.update');
+
+// Laravel 9 way
+Route::get('metatags/defaults', [DefaultsController::class, 'edit'])->name('metatags.defaults');
+Route::post('metatags/defaults', [DefaultsController::class, 'update'])->name('metatags.defaults.update');
+
+
+// Laravel 9 way
+Route::get('metatags/defaults', [DefaultsController::class, 'edit'])->name('metatags.defaults');
+Route::post('metatags/defaults', [DefaultsController::class, 'update'])->name('metatags.defaults.update');
