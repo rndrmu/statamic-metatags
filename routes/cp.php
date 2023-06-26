b6ddcf93d7f05cbabc58a95d6bf959dead5f8d4a
rei <jhallwachs36@gmail.com>
2023-06-26 11:45:04 +0000

    moar updatings

--- routes/cp.php
+++ routes/cp.php
@@ -11,8 +11,8 @@ Route::view('metatags', 'statamic-metatags::index')->name('metatags.index');
 
 
 // Laravel 9 way
-Route::get('metatags/defaults', [DefaultsController::class, 'edit'])->name('metatags.defaults');
-Route::post('metatags/defaults', [DefaultsController::class, 'update'])->name('metatags.defaults.update');
+Route::get('metatags/settings', [SettingsController::class, 'edit'])->name('metatags.settings');
+Route::post('metatags/settings', [SettingsController::class, 'update'])->name('metatags.settings.update');
 
 
 // Laravel 9 way
