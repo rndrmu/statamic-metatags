c5bb4d838434b3e83f130b95ce724f2a5e9a10ab
rei <jhallwachs36@gmail.com>
2023-06-26 10:50:41 +0000

    fixed missing thing lmao

--- routes/cp.php
+++ routes/cp.php
@@ -4,14 +4,26 @@ use Http\Controllers\SettingsController;
 use Http\Controllers\DefaultsController;
 use Tags\MetatagsTags;
 
+/**
+ * 
+ * Route::view('metatags', 'statamic-metatags::index')->name('metatags.index');
+
+Route::get('metatags/settings', [SettingsController::edit])->name('metatags.settings');
+Route::post('metatags/settings', [SettingsController::update])->name('metatags.settings.update');
+
+Route::get('metatags/defaults', [DefaultsController::edit])->name('metatags.defaults');
+Route::post('metatags/defaults', [DefaultsController::update])->name('metatags.defaults.update');
+ * 
+ */
+
 
 // Laravel 9 way
 Route::view('metatags', [MetatagsTags::class, 'index'])->name('metatags.index');
 
 
 // Laravel 9 way
-Route::get('metatags/defaults', [DefaultsController::class, 'edit'])->name('metatags.defaults');
-Route::post('metatags/defaults', [DefaultsController::class, 'update'])->name('metatags.defaults.update');
+Route::get('metatags/settings', [DefaultsController::class, 'edit'])->name('metatags.settings');
+Route::post('metatags/settings', [DefaultsController::class, 'update'])->name('metatags.settings.update');
 
 
 // Laravel 9 way
