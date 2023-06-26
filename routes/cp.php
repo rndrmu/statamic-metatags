ad0cab8a77b73430d73072a3b2f4f0b896d4bdb9
rei <jhallwachs36@gmail.com>
2023-06-26 10:34:53 +0000

    sussy baka

--- routes/cp.php
+++ routes/cp.php
@@ -1,9 +1,12 @@
 <?php
 
+use App\Http\Controllers\SettingsController;
+use App\Http\Controllers\DefaultsController;
+
 Route::view('metatags', 'statamic-metatags::index')->name('metatags.index');
 
-Route::get('metatags/settings', 'SettingsController@edit')->name('metatags.settings');
-Route::post('metatags/settings', 'SettingsController@update')->name('metatags.settings.update');
+Route::get('metatags/settings', [SettingsController::edit])->name('metatags.settings');
+Route::post('metatags/settings', [SettingsController::update])->name('metatags.settings.update');
 
-Route::get('metatags/defaults', 'DefaultsController@edit')->name('metatags.defaults');
-Route::post('metatags/defaults', 'DefaultsController@update')->name('metatags.defaults.update');
+Route::get('metatags/defaults', [DefaultsController::edit])->name('metatags.defaults');
+Route::post('metatags/defaults', [DefaultsController::update])->name('metatags.defaults.update');
