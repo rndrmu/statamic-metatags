8b08c4b0e831ed18950cd19719589c11e1a9bc68
rei <jhallwachs36@gmail.com>
2023-06-26 10:58:25 +0000

    explicit content warning

--- routes/cp.php
+++ routes/cp.php
@@ -1,20 +1,10 @@
 <?php
 
-use Http\Controllers\SettingsController;
-use Http\Controllers\DefaultsController;
-use Tags\MetatagsTags;
+use rndrmu\StatamicMetatags\Http\Controllers\SettingsController;
+use rndrmu\StatamicMetatags\Http\Controllers\DefaultsController;
+use rndrmu\StatamicMetatags\Tags\MetatagsTags;
 
-/**
- * 
- * Route::view('metatags', 'statamic-metatags::index')->name('metatags.index');
 
-Route::get('metatags/settings', [SettingsController::edit])->name('metatags.settings');
-Route::post('metatags/settings', [SettingsController::update])->name('metatags.settings.update');
-
-Route::get('metatags/defaults', [DefaultsController::edit])->name('metatags.defaults');
-Route::post('metatags/defaults', [DefaultsController::update])->name('metatags.defaults.update');
- * 
- */
 
 
 // Laravel 9 way
